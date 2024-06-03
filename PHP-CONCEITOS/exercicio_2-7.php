4635c0843490fe82a3afa1803e5b36ba5d86400d
Matheus Carvalho <the0plxy@gmail.com>
2024-06-03 20:43:15 -0300

    Modificando linhas

--- PHP-CONCEITOS/exercicio_2-7.php
+++ PHP-CONCEITOS/exercicio_2-7.php
@@ -1,8 +1,12 @@
 <?php
-echo "Digite um número para saber a sua tabela de multiplicação: ";
+
+echo "Digite um número para saber a sua tabuada: ";
+
 $numero = trim(fgets(STDIN));
 
 for ($i = 1; $i <= 10; $i++) {
+
     $resultado = $i * $numero;
+
     echo "$numero x $i = $resultado\n";
 }
