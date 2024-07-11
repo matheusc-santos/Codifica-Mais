5b7182e7e6774d14bc5fc7296feaa03041048921
Matheus Carvalho <the0plxy@gmail.com>
2024-07-11 18:30:38 -0300

    Aula 04 SQL

--- SQL-MYSQL/criar_banco_produtos.sql
+++ SQL-MYSQL/criar_banco_produtos.sql
@@ -4,17 +4,17 @@ USE gestao_produtos;
 
 CREATE TABLE produtos (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    sku VARCHAR(30) NOT NULL,
-    descricao VARCHAR(200),
-    categoria VARCHAR(40),
+    nome VARCHAR(255) NOT NULL,
+    sku VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    categoria VARCHAR(100),
     preco DECIMAL(10, 2) NOT NULL,
     unidade_medida VARCHAR(50),
     peso DECIMAL(10, 2),
     quantidade_estoque INT,
-    fabricante VARCHAR(50),
-    fornecedor VARCHAR(50),
-    deleted_at TIMESTAMP DEFAULT NULL,
+    fabricante VARCHAR(255),
+    fornecedor VARCHAR(255),
+    deleted_at TIMESTAMP NULL DEFAULT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT NULL
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
---
+++ SQL-MYSQL/popular_tabelas_auxiliares.sql
@@ -0,0 +1,29 @@
+
+USE gestao_produtos;
+
+INSERT INTO categorias (nome_categoria) VALUES
+('Refrigerante'),
+('Cerveja'),
+('Gin'),
+('Tônica'),
+('Energético');
+
+INSERT INTO unidades_medida (nome_unidade) VALUES
+('ml'),
+('l');
+
+
+INSERT INTO fabricantes (nome_fabricante) VALUES
+('Coca-Cola'),
+('Ambev'),
+('Diageo'),
+('Schweppes'),
+('Red Bull'),
+('Heineken'),
+('Beefeater'),
+('Antarctica'),
+('Monster');
+
+INSERT INTO fornecedores (nome_fornecedor) VALUES
+('Distribuidora XYZ'),
+('Distribuidora ABC');
